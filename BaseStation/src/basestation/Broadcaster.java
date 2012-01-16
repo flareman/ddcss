04a6a3031bd90d340cab5d01b568ab7585bf413a
nflare <nflare@4e50387e-84cd-080c-0224-fe636222e77a>
2012-01-16 01:35:11 +0000

    - Altered WSDL imported to correlate to the 2nd project part.
    Commit 2/2

--- BaseStation/src/basestation/Broadcaster.java
+++ BaseStation/src/basestation/Broadcaster.java
@@ -4,12 +4,6 @@ public class Broadcaster implements Runnable {
     private BaseStationEntity baseStation;
     private Integer keepAliveMsecs;
 
-    private static String ddcUpdateProfile(java.lang.String message) {
-        DDCclient.DDCProfile_Service service = new DDCclient.DDCProfile_Service();
-        DDCclient.DDCProfile port = service.getDDCProfilePort();
-        return port.ddcUpdateProfile(message);
-    }
-
     public Broadcaster(BaseStationEntity parent) {
         this.baseStation = parent;
         this.keepAliveMsecs = parent.getKeepAlive();
@@ -19,15 +13,20 @@ public class Broadcaster implements Runnable {
         while (!Thread.currentThread().isInterrupted()) {
             try {
                 Thread.sleep(keepAliveMsecs);
-                String response = Broadcaster.ddcUpdateProfile(new BSProfileMessage(baseStation).toString());
-                BSMessage rmsg = BSMessage.newMessageFromString(response);
-                if (rmsg.type() != BSMessage.MessageType.OK) throw new Exception("Error: DDC replied \""+rmsg.toString()+"\"");
-                this.baseStation.println("Profile updated; DDC replied \""+rmsg.toString()+"\"");
+                Broadcaster.updateProfile(new BSProfileMessage(baseStation).toString());
+                this.baseStation.println("Profile updated.");
             } catch (InterruptedException e) {
                 return;
             } catch (Exception e) {
                 this.baseStation.println(e.getLocalizedMessage());
             }
         }
-    }    
+    }
+
+    private static void updateProfile(java.lang.String arg0) {
+        ddchannel.WebServicePublisherService service = new ddchannel.WebServicePublisherService();
+        ddchannel.WebServicePublisher port = service.getWebServicePublisherPort();
+        port.updateProfile(arg0);
+    }
+
 }
