ac0e7a2ab02195c136b01200e79c5586a87e1d9e
nflare <nflare@4e50387e-84cd-080c-0224-fe636222e77a>
2012-03-05 02:47:58 +0000

    - Minor revising of the Processor class

--- BaseStation/src/basestation/Processor.java
+++ BaseStation/src/basestation/Processor.java
@@ -1,11 +1,9 @@
 package basestation;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.SocketTimeoutException;
 
 public class Processor extends Thread {
     private Socket client;
